0434b0ecfe31d27ffce0c5c6b9f2ce365f34b682
Klisman Saint <softech.series@gmail.com>
2022-11-16 18:04:33 -0400

    projeto perguntas

---
+++ lib/resposta.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  final String texto;
+  final void Function() quandoSelecionado;
+
+  const Resposta(this.texto, this.quandoSelecionado, {key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white38,
+        ),
+        onPressed: quandoSelecionado,
+        child: Text(texto),
+      ),
+    );
+  }
+}
